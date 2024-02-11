75bc0473c1da1907a1998f5ee2c5e609a577a2fa
Abdulrhaman-Mohamed <abdorrr2015@gmail.com>
2024-02-12 01:40:13 +0200

    remove theme asset

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Here add your Direction of files you would be use tailwind on it 
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
