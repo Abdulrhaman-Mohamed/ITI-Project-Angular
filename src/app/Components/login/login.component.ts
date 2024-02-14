dcf70c1268d22a09811af14fd150d036b4f0e029
AhMeDNeiGm <ahmedneigm@gmail.com>
2024-02-14 21:37:25 +0200

    Login And Registertion Done

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  myForm!: FormGroup;
+  constructor(private _FormBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.creatForm();
+  }
+  creatForm() {
+    this.myForm = this._FormBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+          ),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    }
+  }
+  //#region Email
+  get EmailValid() {
+    return (
+      this.myForm.controls['email'].errors?.['required'] &&
+      this.myForm.controls['email'].dirty
+    );
+  }
+  get EmailValidEmail() {
+    return this.myForm.controls['email'].errors?.['pattern'];
+  }
+  //#endregion Email
+  //#region Password
+  get PasswordValid() {
+    return (
+      this.myForm.controls['password'].errors?.['required'] &&
+      this.myForm.controls['password'].dirty
+    );
+  }
+  get PasswordValidRegex() {
+    return this.myForm.controls['password'].errors?.['pattern'];
+  }
+  //#endregion Password
 }
--- src/app/Components/registertion/registertion.component.ts
+++ src/app/Components/registertion/registertion.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registertion',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registertion.component.html',
   styleUrl: './registertion.component.css',
 })
-export class RegistertionComponent {}
+export class RegistertionComponent implements OnInit {
+  myForm!: FormGroup;
+  constructor(private _FormBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.creatForm();
+  }
+  creatForm() {
+    this.myForm = this._FormBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+          ),
+        ],
+      ],
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    }
+  }
+  //#region First Name
+  get NameValid() {
+    return (
+      this.myForm.controls['firstName'].errors?.['required'] &&
+      this.myForm.controls['firstName'].dirty
+    );
+  }
+  get NameValidMin() {
+    return this.myForm.controls['firstName'].errors?.['minlength'];
+  }
+  //#endregion First Name
+  //#region Last Name
+  get lastNameValid() {
+    return (
+      this.myForm.controls['lastName'].errors?.['required'] &&
+      this.myForm.controls['lastName'].dirty
+    );
+  }
+  get lastNameValidmin() {
+    return this.myForm.controls['lastName'].errors?.['minlength'];
+  }
+  //#endregion Last Name
+  //#region Email
+  get EmailValid() {
+    return (
+      this.myForm.controls['email'].errors?.['required'] &&
+      this.myForm.controls['email'].dirty
+    );
+  }
+  get EmailValidEmail() {
+    return this.myForm.controls['email'].errors?.['pattern'];
+  }
+  //#endregion Email
+  //#region Password
+  get PasswordValid() {
+    return (
+      this.myForm.controls['password'].errors?.['required'] &&
+      this.myForm.controls['password'].dirty
+    );
+  }
+  get PasswordValidRegex() {
+    return this.myForm.controls['password'].errors?.['pattern'];
+  }
+  //#endregion Password
+}
