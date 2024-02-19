3d95b25325b3e314cfea7952775c76f97abbebc4
Ali Ahmed <MrAliAhmedM@gmail.com>
2024-02-20 00:15:16 +0200

    Merge changes from remote main into local main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,37 +12,54 @@ import { AuthLayoutComponent } from './Modules/layouts/auth-layout/auth-layout.c
 import { AdminDashMainLayoutComponent } from './Modules/layouts/admin-dash-main-layout/admin-dash-main-layout.component';
 import { EditAddBlogComponent } from './Modules/Shared/Components/edit-add-blog/edit-add-blog.component';
 import { UserProfileComponent } from './Modules/Admin/components/user-profile/user-profile.component';
+import { BlogDetailsComponent } from './Modules/User/components/blog-details/blog-details.component';
 
 export const routes: Routes = [
     {
-        path: "", component: AuthLayoutComponent,
-        children:
-            [
-                { path: "login", component: LoginPageComponent, title: "Login" },
-                { path: "", redirectTo: "login", pathMatch: "full" },
-                { path: "registration", component: RegisterPageComponent, title: "Registration" },
-            ]
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            { path: 'login', component: LoginPageComponent, title: 'Login' },
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            {
+                path: 'registration',
+                component: RegisterPageComponent,
+                title: 'Registration',
+            },
+        ],
     },
     {
-        path: "dashboard", component: AdminDashMainLayoutComponent,
-        children:
-            [
-                {
-                    path: "", component: AdminDashLayoutComponent,
-                    children:
-                        [
-                            { path: "home", component: AdminDashHomeComponent, title: "Home" },
-                            { path: "", redirectTo: "home", pathMatch: "full" },
-                            { path: "users", component: UsersComponent, title: "Users" },
-                            { path: "blogs", component: BlogsComponent, title: "Blogs" },
-                            { path: "users/:id", component: UserProfileComponent, title: "User profile" },
-                            { path: "users/:id/edit", component: EditUserProfileComponent, title: "Edit profile" },
-                        ]
-                },
-                { path: "blogs/:id", component: EditAddBlogComponent, title: "Blog" },
-                { path: "blogs/add", component: EditAddBlogComponent, title: "Blog" }
-
-            ]
+        path: 'dashboard',
+        component: AdminDashMainLayoutComponent,
+        children: [
+            {
+                path: '',
+                component: AdminDashLayoutComponent,
+                children: [
+                    { path: 'home', component: AdminDashHomeComponent, title: 'Home' },
+                    { path: '', redirectTo: 'home', pathMatch: 'full' },
+                    { path: 'users', component: UsersComponent, title: 'Users' },
+                    {
+                        path: 'blogs',
+                        component: BlogsComponent,
+                        title: 'Blogs',
+                    },
+                    {
+                        path: 'users/:id',
+                        component: UserProfileComponent,
+                        title: 'User profile',
+                    },
+                    {
+                        path: 'users/:id/edit',
+                        component: EditUserProfileComponent,
+                        title: 'Edit profile',
+                    },
+                ],
+            },
+            { path: 'blogs/:id', component: EditAddBlogComponent, title: 'Blog' },
+            { path: 'blogs/add', component: EditAddBlogComponent, title: 'Blog' },
+        ],
     },
-    { path: "**", component: NotFoundComponent, title: "Not found" }
+    { path: 'user', component: BlogDetailsComponent },
+    { path: '**', component: NotFoundComponent, title: 'Not found' },
 ];
