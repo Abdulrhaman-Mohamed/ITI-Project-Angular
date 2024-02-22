9415774015da3cf31338a9f1fa1ec6665c3a498e
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-02-22 11:50:39 +0200

    adding and linking admin dashboard pages

--- src/app/Modules/Shared/Components/not-found/not-found.component.ts
+++ src/app/Modules/Shared/Components/not-found/not-found.component.ts
@@ -1,5 +1,5 @@
 import { RouterModule } from '@angular/router';
-import { AdminDashHomeComponent } from './../../../Admin/components/admin-dash-home/admin-dash-home.component';
+// import { AdminDashHomeComponent } from './../../../Admin/components/admin-dash-home/admin-dash-home.component';
 import { Component } from '@angular/core';
 import { GoToService } from '../../services/go-to.service';
 
@@ -8,12 +8,11 @@ import { GoToService } from '../../services/go-to.service';
   standalone: true,
   imports: [
     RouterModule,
-    AdminDashHomeComponent
+    // AdminDashHomeComponent
   ],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
 export class NotFoundComponent {
-  constructor(public _GoToService: GoToService) { }
-
+  constructor(public _GoToService: GoToService) {}
 }
