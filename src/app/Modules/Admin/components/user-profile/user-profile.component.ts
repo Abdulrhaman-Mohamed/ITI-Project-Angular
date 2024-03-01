d3e31e19c683727b2120f3f7db16907f4ec8af66
AbdalrahmanMG <abdalrahman.m.gomaa@gmail.com>
2024-03-01 08:37:52 +0200

    updating user profile table

--- src/app/Modules/Admin/components/user-profile/user-profile.component.ts
+++ src/app/Modules/Admin/components/user-profile/user-profile.component.ts
@@ -3,17 +3,29 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { GoToService } from '../../../Shared/services/go-to.service';
+import { MessageService, ConfirmationService } from 'primeng/api';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TagModule],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent implements OnInit {
   // * Variables
   user!: User;
+  filteredPosts: any[] = [];
+  posts: any = [];
+  categories: any = [
+    { name: 'Historical' },
+    { name: 'Technology' },
+    { name: 'Education' },
+    { name: 'Personal' },
+    { name: 'General' },
+  ];
 
   private _userId!: string;
   imagePrefix: String = '';
@@ -21,6 +33,8 @@ export class UserProfileComponent implements OnInit {
   constructor(
     private _UserService: UserService,
     private _myActivatedRoute: ActivatedRoute,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
     public _GoToService: GoToService
   ) {}
 
@@ -33,6 +47,80 @@ export class UserProfileComponent implements OnInit {
       console.log({ user });
 
       console.log('constructor profile', this.user);
+    });
+
+    this.getPosts();
+  }
+
+  getPosts() {
+    this._UserService.getAllPosts().subscribe({
+      next: (data) => {
+        this.posts = data.findAll;
+        this.filteredPosts = this.posts;
+        // console.log('stories', this.posts);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getSeverity(category: string) {
+    switch (category) {
+      case 'Historical':
+        return 'warning';
+        break;
+      case 'Technology':
+        return 'success';
+        break;
+      case 'Education':
+        return 'info';
+        break;
+      case 'Personal':
+        return 'danger';
+        break;
+      case 'General':
+        return 'secondary';
+        break;
+
+      default:
+        return '';
+    }
+  }
+
+  deletePost(postId: string) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this post?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-text',
+      rejectButtonStyleClass: 'p-button-text p-button-text',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+
+      accept: () => {
+        this._UserService.distroyPost(postId).subscribe({
+          next: (res) => {
+            console.log(res);
+          },
+          complete: () => {
+            this.getPosts();
+          },
+        });
+
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Confirmed',
+          detail: 'Record deleted',
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'You have rejected',
+        });
+      },
     });
   }
 }
